24d26690f66bb490b49f1b645989a43b1cf1990c
Leo Febey <overclockedamiga@gmail.com>
2014-07-11 23:38:17 +1000

    added

--- index.php
+++ index.php
@@ -12,6 +12,13 @@
 			padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
 		}
 		</style>
+		<meta name='viewport' content='initial-scale=1,maximum-scale=1,user-scalable=no' />
+		<script src='https://api.tiles.mapbox.com/mapbox.js/v1.6.4/mapbox.js'></script>
+		<link href='https://api.tiles.mapbox.com/mapbox.js/v1.6.4/mapbox.css' rel='stylesheet' />
+		<style>
+			body { margin:0; padding:0; }
+			#map { position:absolute; top:0; bottom:0; width:100%; }
+		</style>
 		
 	</head>
 	<body>
@@ -24,7 +31,7 @@
 				<span class="icon-bar"></span>
 			</button>
 			<a class="brand" href="#"><?php echo $title; ?></a>
-			<div class="nav-collapse collapse">
+			<!--<div class="nav-collapse collapse">
 				<ul class="nav">
 				<li class="active"><a href="#">Home</a></li>
 				<li><a href="#about">About</a></li>
@@ -36,8 +43,30 @@
 		</div>
 
 	<div class="container">
-		<h1>Bootstrap starter template</h1>
-		<p>Use this document as a way to quick start any new project.<br> All you get is this message and a barebones HTML document.</p>
+		<div id='map'></div>
+
+		<script>
+			var map = L.mapbox.map('map', 'l33tllama.iobd4k95')
+			.setView([29, -26], 2);
+
+			var marker = L.marker([-73, 40], {
+				icon: L.mapbox.marker.icon({
+				'marker-color': '#f86767'
+				})
+			});
+
+			var t = 0;
+			window.setInterval(function() {
+				// Making a lissajous curve just for fun.
+				// Create your own animated path here.
+				marker.setLatLng(L.latLng(
+					Math.cos(t * 0.5) * 50,
+					Math.sin(t) * 50));
+				t += 0.1;
+			}, 50);
+
+			marker.addTo(map);
+		</script>
 	</div>
 	</body>
 </html>
