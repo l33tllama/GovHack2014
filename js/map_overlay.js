5337dd8b82e273c51344b6c2c64c846e36fd7461
Leo Febey <overclockedamiga@gmail.com>
2014-07-12 09:28:32 +1000

    Added car overlay js

---
+++ js/map_overlay.js
@@ -0,0 +1,87 @@
+function carOverlay(colour, bounds, image, map){
+	this.colour_ = colour;
+	this.bounds_ = bounds;
+	this.map_ = map;
+
+	// a div to store the image's div
+	this.div_ = null;
+
+	this.setMap(map)
+}
+
+carOverlay.prototype.onAdd = function() {
+	var div = document.createElement('div');
+	div.style.borderStyle = "none";
+	div.style.position = 'absolute';
+
+	// Create the img element and attach it to the div.
+	var img = document.createElement('img');
+	img.src = this.image_;
+	img.style.width = '100%';
+	img.style.height = '100%';
+	img.style.position = 'absolute';
+	div.appendChild(img);
+
+	this.div_ = div;
+
+	// Add the element to the "overlayLayer" pane.
+	var panes = this.getPanes();
+	panes.overlayLayer.appendChild(div);
+};
+
+carOverlay.prototype.draw = function() {
+	// We use the south-west and north-east
+	// coordinates of the overlay to peg it to the correct position and size.
+	// To do this, we need to retrieve the projection from the overlay.
+	var overlayProjection = this.getProjection();
+
+	// Retrieve the south-west and north-east coordinates of this overlay
+	// in LatLngs and convert them to pixel coordinates.
+	// We'll use these coordinates to resize the div.
+	var sw = overlayProjection.fromLatLngToDivPixel(this.bounds_.getSouthWest());
+	var ne = overlayProjection.fromLatLngToDivPixel(this.bounds_.getNorthEast());
+
+	// Resize the image's div to fit the indicated dimensions.
+	var div = this.div_;
+	div.style.left = sw.x + 'px';
+	div.style.top = ne.y + 'px';
+	div.style.width = (ne.x - sw.x) + 'px';
+	div.style.height = (sw.y - ne.y) + 'px';
+};
+
+carOverlay.prototype.onRemove = function() {
+	this.div_.parentNode.removeChild(this.div_);
+	this.div_ = null;
+};
+carOverlay.prototype.hide = function() {
+	if (this.div_) {
+		// The visibility property must be a string enclosed in quotes.
+		this.div_.style.visibility = 'hidden';
+	}
+};
+carOverlay.prototype.show = function() {
+	if (this.div_) {
+		this.div_.style.visibility = 'visible';
+	}
+};
+carOverlay.prototype.toggle = function() {
+	if (this.div_) {
+		if (this.div_.style.visibility == 'hidden') {
+			this.show();
+		} else {
+			this.hide();
+		}
+	}
+};
+carOverlay.prototype.toggleDOM = function() {
+	if (this.getMap()) {
+		// Note: setMap(null) calls OverlayView.onRemove()
+		this.setMap(null);
+	} else {
+		this.setMap(this.map_);
+	}
+};
+
+
+
+
