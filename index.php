e36536d0bc9351482dee08f65221fadaefdd9a41
Craig Marshall <craig9@gmail.com>
2014-07-12 06:03:18 +1000

    Added slider, and date selection, working but poor

--- index.php
+++ index.php
@@ -8,14 +8,25 @@
     <meta charset="utf-8">
     <style>
       html, body, #map-canvas {
-        height: 100%;
         margin: 0px;
         padding: 0px
       }
+    html, body {
+        height: 100%;
+    }
+    #map-canvas {
+        height: 90%;
+    }
+    #slider {
+        height: 10%;
+    }
+
     </style>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.0/themes/smoothness/jquery-ui.css">
     <script src="https://maps.googleapis.com/maps/api/js?libraries=visualization"></script>
     <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
     <script src="http://cdnjs.cloudflare.com/ajax/libs/proj4js/2.2.1/proj4-src.js"></script>
+    <script src="http://code.jquery.com/ui/1.11.0/jquery-ui.js"></script>
 
 
     <script>
@@ -49,23 +60,16 @@
             return frequencies[id];
         }
 
-        function initialize() {
-          var mapOptions = {
-            zoom: 7,
-            center: new google.maps.LatLng(-42, 147)
-          };
-          map = new google.maps.Map(document.getElementById('map-canvas'),
-              mapOptions);
-
+        function loadData(position) {
             var data = {
-                sql: 'SELECT * FROM \"e73ea42f-30ee-4a02-a2cb-d3e426c1f0b3\" WHERE \"CRASH_DATE\" LIKE \'2013-03-30%\''
+                sql: 'SELECT * FROM \"e73ea42f-30ee-4a02-a2cb-d3e426c1f0b3\" WHERE \"CRASH_DATE\" LIKE \'' + position + '%\''
             };
-
+            
             $.ajax({
-            url: 'http://data.gov.au/api/action/datastore_search_sql',
-            data: data,
-            dataType: 'jsonp',
-            success: function(data) {
+                url: 'http://data.gov.au/api/action/datastore_search_sql',
+                data: data,
+                dataType: 'jsonp',
+                success: function(data) {
 
                 var fromProj = "+proj=utm +zone=55 +south +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"; 
                 var toProj = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs";
@@ -107,21 +111,61 @@
                         title: 'Description: ' + results[i].DCA + '\n' + 'Severity: ' + results[i].SEVERITY + '\n' + 'No of Vehicles: ' + getFrequency(results[i].ID),
                         icon: getCircle(colour, size)
                     });
-                    //console.log(results[i].SEVERITY + "\n" + getSeverity(results[i].SEVERITY));
-                }
+                    /*marker.setMap(null);
+                    markers.push(marker);
 
+                    function nextMarker(mkNum){
+                        if (mkNum <= markers.length) {;
+                            markers[mkNum].setMap(map);
+                            setTimeout(function() {
+                                //markers[mkNum].setMap(null);
+                                nextMarker(mkNum+1);
+                            }, 100);
+                        }
+                    }
+                    nextMarker(0);*/
+
+                }
             }
           });
+            
+        }
 
+        function initialize() {
 
         }
 
-        google.maps.event.addDomListener(window, 'load', initialize);
+         $(function() {
+            $( "#slider" ).slider({
+                value:0,
+                min: 2004,
+                max: 2014,
+                step: 1,
+                slide: function( event, ui ) {
+                    loadData(ui.value);
+                }
+            });
+          
+          var mapOptions = {
+            zoom: 7,
+            center: new google.maps.LatLng(-42, 147)
+          };
+          map = new google.maps.Map(document.getElementById('map-canvas'),
+              mapOptions);
+
+            //var markers = [];
+
+            loadData(2004);
+        });
+
+
+        //google.maps.event.addDomListener(window, 'load', initialize);
 
     </script>
   </head>
   <body>
     <div id="map-canvas"></div>
+<div id="slider"></div>
   </body>
 </html>
 
