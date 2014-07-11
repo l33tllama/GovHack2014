674d43e9e91189555106420498c8e3fb3ceb8b42
Leo Febey <overclockedamiga@gmail.com>
2014-07-12 00:01:23 +1000

    ajax qery

--- index.php
+++ index.php
@@ -14,7 +14,9 @@
 		</style>
 		<meta name='viewport' content='initial-scale=1,maximum-scale=1,user-scalable=no' />
 		<script src='https://api.tiles.mapbox.com/mapbox.js/v1.6.4/mapbox.js'></script>
-		<link href='https://api.tiles.mapbox.com/mapbox.js/v1.6.4/mapbox.css' rel='stylesheet' />
+		<script src='css/jquery-1.11.1.min.js'></script>
+		<script src='css/bootstrap.js'></script>
+		<!-- <link href='https://api.tiles.mapbox.com/mapbox.js/v1.6.4/mapbox.css' rel='stylesheet' /> -->
 		<style>
 			body { margin:0; padding:0; }
 			#map { position:absolute; top:0; bottom:0; width:100%; }
@@ -46,6 +48,19 @@
 		<div id='map'></div>
 
 		<script>
+			var data = {
+				resource_id: 'e73ea42f-30ee-4a02-a2cb-d3e426c1f0b3', // the resource id
+				limit: 5, // get 5 results
+			q: 'SEVERITY:Fatal' // query for 'jones'
+			};
+			$.ajax({
+				url: 'http://data.gov.au/api/action/datastore_search',
+				data: data,
+				dataType: 'jsonp',
+				success: function(data) {
+				alert('Total results found: ' + data.result.total)
+				}
+			});
 			var map = L.mapbox.map('map', 'l33tllama.iobd4k95')
 			.setView([29, -26], 2);
 
